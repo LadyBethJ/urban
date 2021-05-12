6a6ef6510f7a5f448bc392b06e706bef45d3b1da
LadyBethJ <maribel.juradobaena@gmail.com>
2021-05-12 21:07:53 +0200

    added more fields to Character data model. Created new data class for Location data model

--- app/src/main/java/com/urban/androidhomework/domain/model/Character.kt
+++ app/src/main/java/com/urban/androidhomework/domain/model/Character.kt
@@ -5,6 +5,12 @@ data class Character(
 ) {
     data class CharacterData(
         val id: Int,
-        val name: String
+        val name: String,
+        val status: String,
+        val species: String,
+        val gender: String,
+        val location: Location,
+        val image: String,
+        val created: String
     )
 }
---
+++ app/src/main/java/com/urban/androidhomework/domain/model/Location.kt
@@ -0,0 +1,6 @@
+package com.urban.androidhomework.domain.model
+
+data class Location(
+        val name: String,
+        val url: String
+)
